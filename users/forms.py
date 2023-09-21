6e035da5c78c790c3fe0cf1581ee507dd0f98705
dghnts <mittsulann73@gmail.com>
2023-09-22 01:24:00 +0900

    userモデルの変更

--- users/forms.py
+++ users/forms.py
@@ -2,8 +2,8 @@ from django import froms
 from alllauth.account.forms import SignupForm
 from .models import CustomUser
 
-class SignupForm(SignupForm, UserCreationForm):
+class SignupForm(UserCreationForm):
     
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ("username", "email", "age")
+        fields = ("username", "email")
