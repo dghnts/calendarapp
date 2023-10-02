c48f0cd18ad8959be176a96dda882f3604c22d08
dghnts <mittsulann73@gmail.com>
2023-10-02 11:10:18 +0900

    サイドバーの表示に伴うカレンダーの表示の変更

--- static/common/js/sidebar.js
+++ static/common/js/sidebar.js
@@ -1,11 +1,17 @@
 window.addEventListener("DOMContentLoaded", () => {
     const sidebar_chk = document.getElementById("sidebar_chk");
     const calendar_area = document.querySelector(".calendar_area");
-    console.log(sidebar_chk.checked);
     
     sidebar_chk.addEventListener("click", () => {
-        console.log("こんにちは");
-        calendar_area.style.width="75%";
-        calendar_area.style.marginLeft="25%";
+        sidebar_width = document.querySelector(".sidebar_area").offsetWidth;
+        calendar_width = calendar_area.offsetWidth;
+        console.log(calendar_width-sidebar_width);
+        if(sidebar_chk.checked){
+            calendar_area.style.width=calendar_width-sidebar_width+"px";
+            calendar_area.style.marginLeft=sidebar_width+"px";
+        }else{
+            calendar_area.style.width="100%";
+            calendar_area.style.marginLeft="0%";
+        }
     });
 });
