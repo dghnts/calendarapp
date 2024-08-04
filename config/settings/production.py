5945a62d2f2c7017b02a0e18d8daf382c3a23e09
dghnts <mittsulann73@gmail.com>
2024-08-04 22:51:33 +0900

    production.pyにCSRF_TRUSTED?ORIGINSを追加

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,22 +18,24 @@ else:
 SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 
 
-if not DEBUG:
-    ALLOWED_HOSTS = ["shedule-manager.com"]
 
-    SECRET_KEY = os.environ["SECRET_KEY"]
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.environ["POSTGRESQL_DB_NAME"],
-            "USER": os.environ["POSTGRESQL_DB_USER"],
-            "PASSWORD": os.environ["POSTGRESQL_DB_PASS"],
-            "HOST": "localhost",
-            "PORT": "",
-        }
+ALLOWED_HOSTS = ["shedule-manager.com"]
+
+CSRF_TRUSTED_ORIGINS    = [ "https://shedule-manager.com" ]
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["POSTGRESQL_DB_NAME"],
+        "USER": os.environ["POSTGRESQL_DB_USER"],
+        "PASSWORD": os.environ["POSTGRESQL_DB_PASS"],
+        "HOST": "localhost",
+        "PORT": "",
     }
+}
 
-    STATIC_ROOT = "/var/www/{}/static".format(BASE_DIR.name)
+STATIC_ROOT = "/var/www/{}/static".format(BASE_DIR.name)
 
-    # 下記はファイルのアップロード機能を有する場合のみ
-    MEDIA_ROOT = "/var/www/{}/media".format(BASE_DIR.name)
+# 下記はファイルのアップロード機能を有する場合のみ
+MEDIA_ROOT = "/var/www/{}/media".format(BASE_DIR.name)
