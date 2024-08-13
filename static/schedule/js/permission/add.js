5762b4e56711baa3a44553d9a8a60e38ac862f4b
dghnts <mittsulann73@gmail.com>
2024-08-14 07:27:14 +0900

    権限変更の不具合を修正 #33

--- static/schedule/js/permission/add.js
+++ static/schedule/js/permission/add.js
@@ -1,13 +1,15 @@
-window.addEventListener('load', function (){
-    const permission_form_add   = document.querySelector('#permission_form_add');
-    const permission_form_area  = document.querySelector('#permission_form_area');
+window.addEventListener("load", function () {
+    const permission_form_add = document.querySelector("#permission_form_add");
+    const permission_form_area = document.querySelector(
+        "#permission_form_area"
+    );
     const authority = [
-        { value: "3", read: true, write: true, chat: true},
-        { value: "0", read: true, write: false, chat: false},
-        { value: "1", read: true, write: true, chat: false},
-        { value: "2", read: true, write: false, chat: true},
+        { value: "3", read: true, write: true, chat: true },
+        { value: "0", read: true, write: false, chat: false },
+        { value: "1", read: true, write: true, chat: false },
+        { value: "2", read: true, write: false, chat: true },
     ];
     permission_form_add.addEventListener("click", () => {
-        addPermissionForm(permission_form_area, authority); 
+        addPermissionForm(permission_form_area, authority);
     });
 });
