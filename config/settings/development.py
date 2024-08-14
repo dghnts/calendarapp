313a5754aee463367b4d07de34fb264261e4a526
dghnts <mittsulann73@gmail.com>
2024-08-15 08:34:24 +0900

    SECRET_KEYの修正

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,10 +1,7 @@
 from .base import *
 import os
 
-SECRET_KEY = "b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67"
-
-SECRET_KEY = 'django-insecure-1!-kgq*o@m26z&32k%(*m8ouez6(z7&)_9_*r-5_y)ytsk_=c!'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
